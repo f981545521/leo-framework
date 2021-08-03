4e9c3cf7b5fd219210cac1c4cd3fd1650e7aee0e
youfang <fangyou@guiji.ai>
2021-08-03 15:41:15 +0800

    增加工具

---
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/commons/util/IPUtil.java
@@ -0,0 +1,118 @@
+package cn.acyou.leo.framework.commons.util;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * IP地址工具
+ *
+ * @author youfang
+ * @version [1.0.0, 2020/7/10]
+ **/
+public class IPUtil {
+    /**
+     * 检查IP是否合法
+     *
+     * @param ip ip地址
+     * @return true/false
+     */
+    public static boolean ipValid(String ip) {
+        String regex0 = "(2[0-4]\\d)" + "|(25[0-5])";
+        String regex1 = "1\\d{2}";
+        String regex2 = "[1-9]\\d";
+        String regex3 = "\\d";
+        String regex = "(" + regex0 + ")|(" + regex1 + ")|(" + regex2 + ")|(" + regex3 + ")";
+        regex = "(" + regex + ").(" + regex + ").(" + regex + ").(" + regex + ")";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(ip);
+        return m.matches();
+    }
+
+
+    /**
+     * 获取本地ip 适合windows与linux
+     *
+     * @return {@link String}
+     */
+    public static String getLocalIP() {
+        String defaultLocalIp = "127.0.0.1";
+        List<String> localIps = getLocalIPList();
+        if (CollectionUtils.isNotEmpty(localIps)){
+            return localIps.get(0);
+        }
+        return defaultLocalIp;
+    }
+
+    /**
+     * 获取本地所有网卡ip 适合windows与linux
+     *
+     * @return {@link String}
+     */
+    public static List<String> getLocalIPList() {
+        List<String> localIps = new ArrayList<>();
+        try {
+            //获取本地所有网络接口
+            Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            //遍历枚举中的每一个元素
+            while (en.hasMoreElements()) {
+                NetworkInterface ni = en.nextElement();
+                Enumeration<InetAddress> enumInetAddr = ni.getInetAddresses();
+                while (enumInetAddr.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && !inetAddress.isLinkLocalAddress()
+                            && inetAddress.isSiteLocalAddress()) {
+                        localIps.add(inetAddress.getHostAddress());
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return localIps;
+    }
+
+
+    /**
+     * 获取客户机的ip地址
+     *
+     * @param request 请求
+     * @return {@link String}
+     */
+    public static String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("http_client_ip");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        // 如果是多级代理，那么取第一个ip为客户ip
+        if (ip != null && ip.contains(",")) {
+            ip = ip.substring(ip.lastIndexOf(",") + 1).trim();
+        }
+        return ip;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLocalIP());
+    }
+
+}
---
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/commons/util/MathUtil.java
@@ -0,0 +1,121 @@
+package cn.acyou.leo.framework.commons.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.OptionalDouble;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2020/4/3]
+ **/
+public class MathUtil {
+
+    public static final BigDecimal HUNDRED = new BigDecimal("100");
+
+    public static final BigDecimal PERCENT = new BigDecimal("0.01");
+
+    /**
+     * 计算百分之xx
+     * <pre>
+     *      calculationPercent(2, 5) = 40
+     *      calculationPercent(3, 5) = 60
+     * </pre>
+     *
+     * @param molecule    分子
+     * @param denominator 分母
+     * @return xx%
+     */
+    public static Integer calculationPercent(Integer molecule, Integer denominator) {
+        BigDecimal moleculeDecimal = new BigDecimal(molecule);
+        BigDecimal denominatorDecimal = new BigDecimal(denominator);
+        BigDecimal divide = moleculeDecimal.divide(denominatorDecimal, 2, RoundingMode.HALF_UP);
+        return divide.multiply(HUNDRED).intValue();
+    }
+
+    /**
+     * 计算平均数
+     *
+     * @param sourceNumbers 数据源
+     * @return 平均数
+     */
+    public static Double averageDouble(Collection<Object> sourceNumbers) {
+        OptionalDouble average = Arrays.stream(sourceNumbers.toArray()).mapToDouble(o -> Double.parseDouble(o.toString())).average();
+        if (average.isPresent()) {
+            return average.getAsDouble();
+        } else {
+            throw new IllegalArgumentException("calculation average faild !");
+        }
+    }
+
+    /**
+     * 计算平均数
+     *
+     * @param sourceNumbers 数据源
+     * @return 平均数
+     */
+    public static Double averageDouble(Object[] sourceNumbers) {
+        return averageDouble(Arrays.asList(sourceNumbers));
+    }
+
+    /**
+     * 创建指定长度的最大long类型
+     * <p>
+     * Long.MAX_VALUE: 9223372036854775807
+     * <pre>
+     * createMaxLong(4)     = 9999
+     * createMaxLong(6)     = 999999
+     * createMaxLong(8)     = 99999999
+     * </pre>
+     * @param length 长度 <=18
+     * @return long
+     */
+    public static long createMaxLong(int length) {
+        if (length == 0) {
+            return 0;
+        }
+        if (length > 18) {
+            throw new IllegalArgumentException("length must be less than 18 .");
+        }
+        return Long.parseLong(StringUtil.concatLengthChar(length, '9'));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateQy(2, 0));
+        System.out.println(calculateQy(3, 2));
+        System.out.println(calculateQy(19, 3));
+        System.out.println(calculateQy(0, 19));
+    }
+
+    /**
+     * 计算环比/同比 方法都是一样的，就是上期值不一样
+     *
+     * QoQ计算环比（quarter on quarter）
+     * YoY计算同比（year-over-year）
+     *
+     * 与历史同时期比较，例如2005年7月份与2004年7月份相比称其为同比；
+     * 与上一统计段比较，例如2005年7月份与2005年6月份相比较称其为环比。
+     *
+     * 环比有环比增长速度和环比发展速度两种方法。
+     * 环比即与上期的数量作比较。
+     * 环比增长速度=（本期数－上期数）÷上期数×100%，反映本期比上期增长了多少。
+     * 环比发展速度=本期数÷上期数×100%，环比发展速度是报告期水平与前一期水平之比，反映现象在前后两期的发展变化情况，如：本期销售额为500万，上期销售额为350万。
+     * 环比增长速度=（500－350）÷350×100%=42.86%
+     * 环比发展速度=500÷350×100%=142.86%
+     * 环比增长速度= 环比发展速度－1。
+     *
+     * @param currentNum 本期数
+     * @param lastNum    上期数
+     * @return {@link Integer}
+     */
+    public static Integer calculateQy(Integer currentNum, Integer lastNum) {
+        BigDecimal result = new BigDecimal(currentNum).subtract(new BigDecimal(lastNum));
+        if (lastNum == 0){
+            return 0;
+        }
+        BigDecimal multiply = result.divide(new BigDecimal(lastNum), 2, RoundingMode.UP).multiply(HUNDRED);
+        return multiply.intValue();
+    }
+
+}
