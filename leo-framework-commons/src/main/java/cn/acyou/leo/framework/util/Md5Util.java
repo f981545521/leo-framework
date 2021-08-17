0cda8d1ebda7910741a1f10ae769ae40039fa9ea
youfang <fangyou@guiji.ai>
2021-08-17 09:44:21 +0800

    Md5计算

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/Md5Util.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/Md5Util.java
@@ -1,21 +1,77 @@
 package cn.acyou.leo.framework.util;
 
+import com.alibaba.fastjson.JSON;
 
+import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * MD5算法
+ *
+ * @author fangyou
+ * @since 2021/08/17
  */
 public class Md5Util {
 
-    public static String getMD5(String info) {
+    /**
+     * 得到对象的md5值
+     * <p>原理：转成JSON字符串后再计算MD5值</p>
+     *
+     * @param serialObj 串行obj
+     * @return {@link String}
+     */
+    public static String getObjectMd5(Serializable... serialObj) {
+        StringBuilder sb = new StringBuilder();
+        for (Serializable obj : serialObj) {
+            sb.append(JSON.toJSONString(obj));
+        }
+        return md5(sb.toString());
+    }
+
+    /**
+     * 计算加盐md5
+     *
+     * @param sourceStr 源str
+     * @param salt      盐
+     * @return {@link String}
+     */
+    public static String md5(String sourceStr, String salt) {
+        return md5(sourceStr + salt);
+    }
+
+
+    /**
+     * 计算字符串的md5值（32位 数字+大写字母）
+     *
+     * @param sourceStr 源str
+     * @return {@link String}
+     */
+    public static String md5Upper(String sourceStr) {
+        return md5(sourceStr).toUpperCase();
+    }
+
+    /**
+     * 计算字符串的md5值（32位 数字+小写字母）
+     *
+     * @param sourceStr 源str
+     * @return {@link String}
+     */
+    public static String md5(String sourceStr) {
+        return md5(sourceStr.getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * 计算byte数组的md5值（32位 数字+小写字母）
+     *
+     * @param bytes byte数组
+     * @return {@link String}
+     */
+    public static String md5(byte[] bytes) {
         try {
             MessageDigest md5 = MessageDigest.getInstance("MD5");
-            md5.update(info.getBytes(StandardCharsets.UTF_8));
+            md5.update(bytes);
             byte[] encryption = md5.digest();
-
             StringBuilder strBuf = new StringBuilder();
             for (byte b : encryption) {
                 if (Integer.toHexString(0xff & b).length() == 1) {
@@ -30,69 +86,9 @@ public class Md5Util {
         }
     }
 
-    public static String string2Md5(String plainText) {
-        StringBuilder buf = null;
-        String bufInfo = "";
-        try {
-            if (null != plainText) {
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                md.update(plainText.getBytes());
-                byte[] b = md.digest();
-                int i;
-                buf = new StringBuilder("");
-                for (byte value : b) {
-                    i = value;
-                    if (i < 0) {
-                        i += 256;
-                    }
-                    if (i < 16) {
-                        buf.append("0");
-                    }
-                    buf.append(Integer.toHexString(i));
-                }
-            }
-            if (null != buf) {
-                bufInfo = buf.toString().substring(8, 24);// 16位的加密
-            }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return bufInfo;
-    }
-
-
-    public static String string2Md5for32(String plainText) {
-        StringBuilder buf = null;
-        String bufInfo = "";
-        try {
-            if (null != plainText) {
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                md.update(plainText.getBytes());
-                byte[] b = md.digest();
-                int i;
-                buf = new StringBuilder("");
-                for (byte value : b) {
-                    i = value;
-                    if (i < 0) {
-                        i += 256;
-                    }
-                    if (i < 16) {
-                        buf.append("0");
-                    }
-                    buf.append(Integer.toHexString(i));
-                }
-            }
-            if (null != buf) {
-                bufInfo = buf.toString().toUpperCase();// 32位的加密
-            }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return bufInfo;
-    }
-
-
     public static void main(String[] args) {
-        System.out.println(string2Md5for32("123456789"));
+        System.out.println(md5("admin"));
+        System.out.println(md5Upper("admin"));
+        System.out.println(md5("admin", "admin123"));
     }
 }
