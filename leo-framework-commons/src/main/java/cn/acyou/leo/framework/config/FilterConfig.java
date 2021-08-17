b37ea36857bcb3393dc477a5d8dddf5a060da633
youfang <youfang@acyou.cn>
2021-08-17 21:38:23 +0800

    使用jsoup的xss

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/FilterConfig.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/FilterConfig.java
@@ -1,6 +1,9 @@
 package cn.acyou.leo.framework.config;
 
+import cn.acyou.leo.framework.prop.XssProperty;
+import cn.acyou.leo.framework.util.StringUtil;
 import cn.acyou.leo.framework.xss.XssFilter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,7 @@ import java.util.Map;
  * @version [1.0.0, 2020/7/13]
  **/
 @Configuration
+@EnableConfigurationProperties(value = XssProperty.class)
 public class FilterConfig {
 
     /**
@@ -22,7 +26,7 @@ public class FilterConfig {
      * @return FilterRegistrationBean
      */
     @Bean
-    public FilterRegistrationBean<?> xssFilterRegistration() {
+    public FilterRegistrationBean<?> xssFilterRegistration(XssProperty xssProperty) {
         FilterRegistrationBean<XssFilter> registration = new FilterRegistrationBean<>();
         registration.setDispatcherTypes(DispatcherType.REQUEST);
         registration.setFilter(new XssFilter());
@@ -30,8 +34,12 @@ public class FilterConfig {
         registration.setName("xssFilter");
         registration.setOrder(Integer.MAX_VALUE);
         Map<String, String> initParameters = new HashMap<>();
-        initParameters.put("excludes", "");
-        initParameters.put("enabled", "true");
+        if (StringUtil.isNotNullOrBlank(xssProperty.getExcludes())) {
+            initParameters.put("excludes", xssProperty.getExcludes());
+        }
+        if (StringUtil.isNotNullOrBlank(xssProperty.getEnabled())) {
+            initParameters.put("enabled", xssProperty.getEnabled());
+        }
         registration.setInitParameters(initParameters);
         return registration;
     }
---
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/prop/XssProperty.java
@@ -0,0 +1,23 @@
+package cn.acyou.leo.framework.prop;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2021-8-17]
+ **/
+@Data
+@ConfigurationProperties(prefix = "xss")
+public class XssProperty {
+
+    /**
+     * 开启XSS过滤
+     */
+    private String enabled = "true";
+    /**
+     * XSS过滤排除URL
+     */
+    private String excludes = "";
+
+}
