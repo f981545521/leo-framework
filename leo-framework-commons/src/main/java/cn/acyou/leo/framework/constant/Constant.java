60c044eb9883fd774eb97661d9eefffe563dc6ad
youfang <fangyou@guiji.ai>
2021-09-07 14:50:03 +0800

    维护通用常量

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/constant/Constant.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/constant/Constant.java
@@ -1,6 +1,8 @@
 package cn.acyou.leo.framework.constant;
 
 /**
+ * 通用常量类
+ *
  * @author youfang
  * @version [1.0.0, 2020/4/1]
  **/
@@ -9,16 +11,29 @@ public class Constant {
     public static final Integer FLAG_TRUE_1 = 1;
 
     public static final Integer CONS_0 = 0;
-    public static final Long CONS_LONG_0 = 0L;
     public static final Integer CONS_1 = 1;
     public static final Integer CONS_2 = 2;
     public static final Integer CONS_3 = 3;
     public static final Integer CONS_4 = 4;
     public static final Integer CONS_5 = 5;
+    public static final Integer CONS_6 = 6;
+    public static final Integer CONS_7 = 7;
+    public static final Integer CONS_8 = 8;
+    public static final Integer CONS_9 = 9;
     public static final Integer CONS_99 = 99;
+    public static final Long CONS_LONG_0 = 0L;
+    public static final Long CONS_LONG_1 = 1L;
+    public static final Long CONS_LONG_2 = 2L;
+    public static final Long CONS_LONG_3 = 3L;
+    public static final Long CONS_LONG_4 = 4L;
+    public static final Long CONS_LONG_5 = 5L;
+    public static final Long CONS_LONG_6 = 6L;
+    public static final Long CONS_LONG_7 = 7L;
+    public static final Long CONS_LONG_8 = 8L;
+    public static final Long CONS_LONG_9 = 9L;
 
     public static final Integer NORMAL = 1;
-    public static final Integer DISABLED = 0;
     public static final Integer ENABLE = 1;
+    public static final Integer DISABLED = 0;
 }
 
