99314c00bccfcad01b39347925583a98fb924567
youfang <fangyou@guiji.ai>
2021-09-27 17:04:07 +0800

    CorsFilter

---
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/CorsFilter.java
@@ -0,0 +1,69 @@
+package cn.acyou.leo.framework.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.web.cors.*;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 单独配置：Cors Filter
+ *
+ * 使用CorsFilter解决解决跨域
+ * 参考：{@link org.springframework.web.filter.CorsFilter}
+ * @author youfang
+ * @version [1.0.0, 2020/7/28]
+ **/
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+    private final CorsConfigurationSource configSource;
+
+    private CorsProcessor processor = new DefaultCorsProcessor();
+
+    public CorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        List<String> list = new ArrayList<>();
+        list.add("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(list);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        this.configSource = source;
+    }
+
+
+    /**
+     * Configure a custom {@link CorsProcessor} to use to apply the matched
+     * {@link CorsConfiguration} for a request.
+     * <p>By default {@link DefaultCorsProcessor} is used.
+     */
+    public void setCorsProcessor(CorsProcessor processor) {
+        Assert.notNull(processor, "CorsProcessor must not be null");
+        this.processor = processor;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        CorsConfiguration corsConfiguration = this.configSource.getCorsConfiguration(request);
+        boolean isValid = this.processor.processRequest(corsConfiguration, request, response);
+        if (!isValid || CorsUtils.isPreFlightRequest(request)) {
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
