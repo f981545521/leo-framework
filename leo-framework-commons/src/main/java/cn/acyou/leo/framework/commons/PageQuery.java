ee621ce2d1fc5d9086e1736b6856083d99ca8fdf
youfang <youfang@acyou.cn>
2021-11-13 16:12:33 +0800

    注释补充

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/commons/PageQuery.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/commons/PageQuery.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 /**
+ * 分页查询工具
+ *
  * @author fangyou
  * @version [1.0.0, 2021-08-03 16:57]
  */
@@ -22,14 +24,14 @@ public class PageQuery {
      * 使用 springframework.data.domain.Page 时转换为 PageData
      *
      * <p>example:</p>
-     *  <pre>
-     ** Page<ProductEo> users = productEsRepository.findAll(PageRequest.of(1, 20));
-     ** PageData<ProductEo> pageData = PageQuery.convert(user);
-     *  </pre>
+     * <pre>
+     * Page&lt;ProductEo&gt; users = productEsRepository.findAll(PageRequest.of(1, 20));
+     * PageData&lt;ProductEo&gt; pageData = PageQuery.convert(user);
+     * </pre>
      *
-     * @param <T> 类型
-     * @param springPage spring data的page
-     * @return PageData
+     * @param <T> 数据类型
+     * @param springPage spring data的分页对象
+     * @return 分页数据
      */
     public static <T> PageData<T> convert(Page<T> springPage) {
         Integer pageNum = springPage.getNumber() != 0 ? springPage.getNumber() : 1;
@@ -44,14 +46,14 @@ public class PageQuery {
      *
      * <p>example:</p>
      * <pre>
-     ** PageHelper.startPage(req.getPageNum(), req.getPageSize());
-     ** List<MarketingProductVo> marketingProductList = marketingProductMapper.selectMarketingProduct(req);
-     ** PageData<MarketingProductVo> PageData = PageData.convert(new PageInfo<>(marketingProductList));
+     * PageHelper.startPage(req.getPageNum(), req.getPageSize());
+     * List&lt;MarketingProductVo&gt; marketingProductList = marketingProductMapper.selectMarketingProduct(req);
+     * PageData&lt;MarketingProductVo&gt; PageData = PageData.convert(new PageInfo&lt;&gt;(marketingProductList));
      * </pre>
      *
      * @param pageInfo pageHelper 分页对象
-     * @param <T>      具体类型
-     * @return PageData
+     * @param <T>      数据类型
+     * @return 分页数据
      */
     public static <T> PageData<T> convert(PageInfo<T> pageInfo) {
         //这里没有数据的时候pageNum是0
@@ -67,9 +69,9 @@ public class PageQuery {
      *
      * @param <T> 目标类型
      * @param <E> pageInfo类型
-     * @param pageInfo 页面信息
+     * @param pageInfo 分页信息
      * @param tarClass 目标类型Class
-     * @return PageData
+     * @return 分页数据
      */
     public static <E, T> PageData<T> convert(PageInfo<E> pageInfo, Class<T> tarClass) {
         //这里没有数据的时候pageNum是0
@@ -85,14 +87,14 @@ public class PageQuery {
      *
      * <p>example:</p>
      * <pre>
-     ** PageHelper.startPage(req.getPageNum(), req.getPageSize());
-     ** List<MarketingProductVo> marketingProductList = marketingProductMapper.selectMarketingProduct(req);
-     ** PageData<MarketingProductVo> PageData = PageData.convert(marketingProductList);
+     * PageHelper.startPage(req.getPageNum(), req.getPageSize());
+     * List&lt;MarketingProductVo&gt; marketingProductList = marketingProductMapper.selectMarketingProduct(req);
+     * PageData&lt;MarketingProductVo&gt; PageData = PageQuery.convert(marketingProductList);
      * </pre>
      *
      * @param <T>      具体类型
      * @param dataList dataList 数据List
-     * @return PageData
+     * @return 分页数据
      */
     public static <T> PageData<T> convert(List<T> dataList) {
         PageInfo<T> pageInfo = new PageInfo<>(dataList);
@@ -106,13 +108,15 @@ public class PageQuery {
      * <p>example:</p>
      *
      * <pre>
-     ** PageData<Student> convert2 =  PageData.startPage(pageNum, pageSize).selectMapper(studentService.selectAll());
-     ** PageData<StudentVo> convertType =  PageData.startPage(pageNum, pageSize).selectMapper(studentService.selectAll(), StudentVo.class);
+     *  PageData&lt;Student&gt; page1 =  PageData.startPage(pageNum, pageSize)
+     *                 .selectMapper(studentService.selectAll());
+     *  PageData&lt;StudentVo&gt; page2 =  PageData.startPage(pageNum, pageSize)
+     *                 .selectMapper(studentService.selectAll(), StudentVo.class);
      * </pre>
      *
      * @param pageNum  页码，从1开始
      * @param pageSize 页面大小
-     * @return PageData
+     * @return 开启查询
      */
     public static PageQuery startPage(Integer pageNum, Integer pageSize) {
         judgeNotNull(pageNum, pageSize);
@@ -128,7 +132,7 @@ public class PageQuery {
      * @param pageSizeZero true     时支持pageSize=0查全部
      *                     false    时pageSize=0没有结果
      *                     null     时用按照默认{false}配置
-     * @return PageData
+     * @return 开启查询
      */
     public static PageQuery startPage(Integer pageNum, Integer pageSize, Boolean pageSizeZero) {
         judgeNotNull(pageNum, pageSize);
@@ -142,7 +146,7 @@ public class PageQuery {
      * @param pageNum  页码，从1开始
      * @param pageSize 页面大小
      * @param orderBy  排序
-     * @return PageData
+     * @return 开启查询
      */
     public static PageQuery startPage(Integer pageNum, Integer pageSize, String orderBy) {
         judgeNotNull(pageNum, pageSize);
@@ -154,7 +158,7 @@ public class PageQuery {
      * 参考： {@link #startPage(Integer, Integer)}
      *
      * @param pageSo 分页参数
-     * @return PageData
+     * @return 开启查询
      */
     public static PageQuery startPage(PageSo pageSo) {
         return startPage(pageSo.getPageNum(), pageSo.getPageSize(), SqlUtil.convertOrderBy(pageSo));
@@ -167,7 +171,7 @@ public class PageQuery {
      * @param pageSizeZero true     时支持pageSize=0查全部
      *                     false    时pageSize=0没有结果
      *                     null     时用按照默认{false}配置
-     * @return PageData
+     * @return 开启查询
      */
     public static PageQuery startPage(PageSo pageSo, Boolean pageSizeZero) {
         judgeNotNull(pageSo.getPageNum(), pageSo.getPageSize());
@@ -177,11 +181,11 @@ public class PageQuery {
     }
 
     /**
-     * 分页查询
+     * 执行查询
      *
      * @param queryList 查询结果
      * @param <T>      泛型
-     * @return PageData
+     * @return 分页数据
      */
     public <T> PageData<T> selectMapper(List<T> queryList) {
         PageInfo<T> pageInfo = new PageInfo<>(queryList);
@@ -189,13 +193,13 @@ public class PageQuery {
     }
 
     /**
-     * 分页查询（包含类型转换）
+     * 执行查询（包含类型转换）
      *
      * @param <T>      目标类型
      * @param <E>      结果类型
      * @param queryList 查询结果
      * @param tarClass  模板类型
-     * @return PageData
+     * @return 分页数据
      */
     public <E, T> PageData<T> selectMapper(List<E> queryList, Class<T> tarClass) {
         PageInfo<E> pageInfo = new PageInfo<>(queryList);
@@ -204,7 +208,7 @@ public class PageQuery {
 
     /**
      * 返回空分页
-     * @return 空分页
+     * @return 空分页数据
      */
     public static <T> PageData<T> empty() {
         PageData<T> pageData = new PageData<>(1, 10);
@@ -215,7 +219,7 @@ public class PageQuery {
     /**
      * 返回空分页 （根据 分页大小）
      * @param pageSize 分页大小
-     * @return 空分页
+     * @return 空分页数据
      */
     public static <T> PageData<T> empty(int pageSize) {
         PageData<T> pageData = new PageData<>(1, pageSize);
