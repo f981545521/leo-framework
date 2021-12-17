c94aa4d00c379c196e026c55393a7930a1f9142b
youfang <fangyou@guiji.ai>
2021-12-17 14:04:18 +0800

    优化

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/advisor/RedisLockAnnotationBeanPostProcessor.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/advisor/RedisLockAnnotationBeanPostProcessor.java
@@ -2,6 +2,7 @@ package cn.acyou.leo.framework.advisor;
 
 import cn.acyou.leo.framework.util.redis.RedisUtils;
 import org.springframework.aop.framework.autoproxy.AbstractBeanFactoryAwareAdvisingPostProcessor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,8 +23,9 @@ public class RedisLockAnnotationBeanPostProcessor extends AbstractBeanFactoryAwa
     public void setBeanFactory(BeanFactory beanFactory) {
         super.setBeanFactory(beanFactory);
         RedisLockAnnotationInterceptor interceptor = new RedisLockAnnotationInterceptor(redisUtils, redisLockSource);
+
         RedisLockPointCut redisLockPointCut = new RedisLockPointCut(redisLockSource);
-        this.advisor = new RedisLockAnnotationAdvisor(interceptor, redisLockPointCut);
+        this.advisor = new DefaultPointcutAdvisor(redisLockPointCut, interceptor);
     }
 
 }
