e8fdf3a7dda0ce476162172d6a504268f88252fb
youfang <fangyou@guiji.ai>
2021-12-24 10:34:24 +0800

    优化

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/constant/Constant.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/constant/Constant.java
@@ -10,9 +10,11 @@ import cn.acyou.leo.framework.commons.EnumTool;
  * @version [1.0.0, 2020/4/1]
  **/
 public class Constant {
+    /*  ============== 通用 0/1 =================== */
     public static final Integer FLAG_FALSE_0 = 0;
     public static final Integer FLAG_TRUE_1 = 1;
 
+    /*  ============= 通用 Integer 常量 ============ */
     public static final Integer CONS_0 = 0;
     public static final Integer CONS_1 = 1;
     public static final Integer CONS_2 = 2;
@@ -24,6 +26,7 @@ public class Constant {
     public static final Integer CONS_8 = 8;
     public static final Integer CONS_9 = 9;
     public static final Integer CONS_99 = 99;
+    /*  ============ 通用 Long 常量 ================ */
     public static final Long CONS_LONG_0 = 0L;
     public static final Long CONS_LONG_1 = 1L;
     public static final Long CONS_LONG_2 = 2L;
@@ -35,21 +38,48 @@ public class Constant {
     public static final Long CONS_LONG_8 = 8L;
     public static final Long CONS_LONG_9 = 9L;
 
+    /*  ============== 通用常量 ==================== */
     public static final Integer NORMAL = 1;
     public static final Integer ENABLE = 1;
     public static final Integer DISABLED = 0;
+
+    /*  ============= 通用 int 常量 ================ */
+    public static class INT {
+        public static final int CONS_0 = 0;
+        public static final int CONS_1 = 1;
+        public static final int CONS_2 = 2;
+        public static final int CONS_3 = 3;
+        public static final int CONS_4 = 4;
+        public static final int CONS_5 = 5;
+        public static final int CONS_6 = 6;
+        public static final int CONS_7 = 7;
+        public static final int CONS_8 = 8;
+        public static final int CONS_9 = 9;
+        public static final int CONS_10 = 10;
+        public static final int CONS_20 = 20;
+        public static final int CONS_30 = 30;
+        public static final int CONS_40 = 40;
+        public static final int CONS_50 = 50;
+        public static final int CONS_60 = 60;
+        public static final int CONS_70 = 70;
+        public static final int CONS_80 = 80;
+        public static final int CONS_90 = 90;
+        public static final int CONS_100 = 100;
+    }
 
     /**
      * 请求头header名称
      */
     public static final String TOKEN_NAME = "Authentication";
-
+    /**
+     * 系统角色
+     */
     public static class SysRole {
         public static final String SUPER_ADMIN = "super_admin";
 
         public static final EnumEntity SUPER_ADMIN_ENTITY = new EnumEntity(SUPER_ADMIN, "超级管理员");
 
-        public static EnumTool tool(){
+        public static EnumTool tool() {
             return EnumTool.newInstance(SysRole.class);
         }
     }
