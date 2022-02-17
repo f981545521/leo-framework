d199b0f63c28b80b611d00e2111809540274a730
youfang <fangyou@guiji.ai>
2022-02-17 09:13:41 +0800

    日志优化

--- leo-pay/src/main/java/cn/acyou/leo/pay/LeoPayApplication.java
+++ leo-pay/src/main/java/cn/acyou/leo/pay/LeoPayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeoPayApplication {
 
     public static void main(String[] args) {
+        System.setProperty("pagehelper.banner", "false");
         SpringApplication.run(LeoPayApplication.class, args);
     }
 
