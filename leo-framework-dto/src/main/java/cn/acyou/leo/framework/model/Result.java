52362db6cc8095f3d02de8e88bec55d4e7832098
youfang <fangyou@guiji.ai>
2022-04-08 15:53:19 +0800

    修改值

--- leo-framework-dto/src/main/java/cn/acyou/leo/framework/model/Result.java
+++ leo-framework-dto/src/main/java/cn/acyou/leo/framework/model/Result.java
@@ -11,7 +11,7 @@ public class Result<T> extends DTO {
     private String message;
     private T data;
 
-    private static final int SUCCESS_CODE = 0;
+    private static final int SUCCESS_CODE = 200;
     private static final String SUCCESS_MESSAGE = "处理成功";
     private static final int ERROR_CODE = 500;
     private static final String ERROR_MESSAGE = "未知异常，请联系管理员";
