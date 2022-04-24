cda684a8876a26a6bcbf99a2ad71801db54b4998
youfang <fangyou@guiji.ai>
2022-04-24 21:00:24 +0800

    幂等校验优化

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/commons/SysCommonController.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/commons/SysCommonController.java
@@ -1,17 +1,20 @@
 package cn.acyou.leo.framework.commons;
 
 import cn.acyou.leo.framework.constant.CommonErrorEnum;
+import cn.acyou.leo.framework.exception.ServiceException;
 import cn.acyou.leo.framework.model.Result;
 import cn.acyou.leo.framework.util.IdUtil;
 import cn.acyou.leo.framework.util.redis.RedisUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -47,13 +50,28 @@ public class SysCommonController {
         return Result.error(CommonErrorEnum.E_UNAUTHORIZED);
     }
 
-    @GetMapping(value = "/getSequence")
-    @ApiOperation(value = "获取幂等序列")
+    @GetMapping(value = "/idempotent")
+    @ApiOperation(value = "获取幂等序列 prefix前缀（必传） sequence：空时生成，非空时判断 ")
     @ResponseBody
-    public Result<String> getSequence(String prefix) {
-        String uuid = IdUtil.uuidStrWithoutLine();
-        redisUtils.set(RedisKeyConstant.AUTO_IDEMPOTENT_SEQUENCE + prefix + uuid, "1", 1, TimeUnit.HOURS);
-        return Result.success(uuid);
+    public Result<String> idempotent(@RequestParam("prefix") String prefix, String sequence) {
+        if (sequence == null || StringUtils.isBlank(sequence)) {
+            String uuid = IdUtil.uuidStrWithoutLine();
+            redisUtils.set(RedisKeyConstant.AUTO_IDEMPOTENT_SEQUENCE + prefix + uuid, "1", 1, TimeUnit.HOURS);
+            return Result.success(uuid);
+        } else {
+            String key = RedisKeyConstant.AUTO_IDEMPOTENT_SEQUENCE + prefix + sequence;
+            String v = null;
+            try {
+                v = redisUtils.get(key);
+                if (!org.springframework.util.StringUtils.hasText(v)) {
+                    throw new ServiceException(CommonErrorEnum.REPETITIVE_OPERATION);
+                }
+                return Result.success();
+            } finally {
+                //使用过就删除序列
+                redisUtils.delete(key);
+            }
+        }
     }
 
     @GetMapping(value = "/error")
