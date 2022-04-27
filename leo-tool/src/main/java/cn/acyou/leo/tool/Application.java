775d197a36da08eb8e7a31a8e9f1c3af961cc3b6
youfang <fangyou@guiji.ai>
2022-04-27 15:01:17 +0800

    计划任务

--- leo-tool/src/main/java/cn/acyou/leo/tool/Application.java
+++ leo-tool/src/main/java/cn/acyou/leo/tool/Application.java
@@ -1,9 +1,11 @@
 package cn.acyou.leo.tool;
 
+import cn.acyou.leo.framework.auto.EnableLeoFramework;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableLeoFramework
 @SpringBootApplication(scanBasePackages = "cn.acyou.leo")
 @MapperScan("cn.acyou.leo.tool.mapper")
 public class Application {
---
+++ leo-tool/src/main/java/cn/acyou/leo/tool/task/MyDynamicTask.java
@@ -0,0 +1,17 @@
+package cn.acyou.leo.tool.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2022/4/27 14:19]
+ **/
+@Slf4j
+@Component
+public class MyDynamicTask extends AbstractTaskParent {
+    @Override
+    public void run(String params) {
+        log.info("执行了MyDynamicTask...");
+    }
+}
