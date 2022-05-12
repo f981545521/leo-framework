df44c341f0725073f303816bcef0ced7c299fe78
youfang <fangyou@guiji.ai>
2022-05-12 11:25:00 +0800

    集成测试

--- leo-pay/src/main/java/cn/acyou/leo/pay/LeoPayApplication.java
+++ leo-pay/src/main/java/cn/acyou/leo/pay/LeoPayApplication.java
@@ -1,15 +1,17 @@
 package cn.acyou.leo.pay;
 
-import org.mybatis.spring.annotation.MapperScan;
+import cn.acyou.leo.framework.auto.EnableLeoFramework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "cn.acyou.leo")
-@MapperScan("cn.acyou.leo.pay.mapper")
+@EnableLeoFramework
+//@MapperScan("cn.acyou.leo.pay.mapper")
+@SpringBootApplication(scanBasePackages = "cn.acyou.leo", exclude = {RedisAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class LeoPayApplication {
 
     public static void main(String[] args) {
-        System.setProperty("pagehelper.banner", "false");
         SpringApplication.run(LeoPayApplication.class, args);
     }
 
