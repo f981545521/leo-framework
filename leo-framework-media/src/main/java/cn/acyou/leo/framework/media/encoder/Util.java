467c6aabb4c20bfbe88bb0a486ad328f16f39b8f
youfang <fangyou@guiji.ai>
2023-11-22 14:12:42 +0800

    保留2位小数

--- leo-framework-media/src/main/java/cn/acyou/leo/framework/media/encoder/Util.java
+++ leo-framework-media/src/main/java/cn/acyou/leo/framework/media/encoder/Util.java
@@ -130,10 +130,10 @@ class Util {
             return String.format("%.1f GB", (float) size / gb);
         } else if (size >= mb) {
             float f = (float) size / mb;
-            return String.format(f > 100 ? "%.0f MB" : "%.1f MB", f);
+            return String.format(f > 100 ? "%.0f MB" : "%.2f MB", f);
         } else if (size >= kb) {
             float f = (float) size / kb;
-            return String.format(f > 100 ? "%.0f KB" : "%.1f KB", f);
+            return String.format(f > 100 ? "%.0f KB" : "%.2f KB", f);
         } else {
             return String.format("%d B", size);
         }
