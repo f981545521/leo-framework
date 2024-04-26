379006d61cf92136e038615187462130a1cb566e
youfang <fangyou@guiji.ai>
2024-04-26 14:11:00 +0800

    tests

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/test/TestUtils.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/test/TestUtils.java
@@ -2,27 +2,37 @@ package cn.acyou.leo.framework.test;
 
 import cn.acyou.leo.framework.exception.AssertException;
 import cn.acyou.leo.framework.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
 
+import java.io.File;
 import java.util.Properties;
 
 /**
  * @author youfang
  * @version [1.0.0, 2024/4/25 11:09]
  **/
+@Slf4j
 public class TestUtils {
 
     public static String DEFAULT_PATH = "D:\\workspace\\work\\private_account.properties";
+    public static String DEFAULT_PATH_2 = "E:\\workspace\\work\\private_account.properties";
 
     public static void loadExtendProperties() {
         loadExtendProperties(DEFAULT_PATH);
+        loadExtendProperties(DEFAULT_PATH_2);
     }
 
     public static void loadExtendProperties(String path) {
         try {
-            Properties properties = System.getProperties();
-            properties.load(new FileSystemResource(path).getInputStream());
-            System.setProperties(properties);
+            if (new File(path).exists()) {
+                Properties properties = System.getProperties();
+                properties.load(new FileSystemResource(path).getInputStream());
+                System.setProperties(properties);
+                log.info("加载配置成功 {}", path);
+            }else {
+                log.info("加载配置失败 {}", path);
+            }
         }catch (Exception e) {
             //ignore
         }
