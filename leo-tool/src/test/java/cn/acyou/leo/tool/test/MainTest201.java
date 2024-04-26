ce833e2119a2c78e3e21008398a52ac1b96c7901
youfang <fangyou@guiji.ai>
2024-04-26 14:04:06 +0800

    tests

--- leo-tool/src/test/java/cn/acyou/leo/tool/test/MainTest201.java
+++ leo-tool/src/test/java/cn/acyou/leo/tool/test/MainTest201.java
@@ -19,14 +19,13 @@ import java.util.concurrent.atomic.AtomicLong;
  * @version [1.0.0, 2024/4/26 9:14]
  **/
 @Slf4j
-public class MainTest200 {
+public class MainTest201 {
     private static final AtomicLong idGenerator = new AtomicLong(1);
     private static final JSONArray jsonArray = new JSONArray();
 
     public static void main(String[] args) {
         TestUtils.loadExtendProperties();
         File path = new File("D:\\TranslateVideos");
-        File indexPath = new File("D:\\TranslateVideos_index");
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("id", idGenerator.getAndAdd(1));
         jsonObject.put("parentId", 0);
@@ -35,15 +34,11 @@ public class MainTest200 {
         jsonObject.put("open", true);
         jsonArray.add(jsonObject);
 
-        copyIndexName(path, indexPath, jsonObject);
+        copyIndexName(path, jsonObject);
 
         System.out.println("+++++++++++++  解析文件成功  +++++++++++++");
         String content = "window.ztreeConfig = '" + UrlUtil.encode( JSON.toJSONString(jsonArray)) + "'";
         final byte[] bytes = content.getBytes();
-        File file = new File("D:\\TranslateVideos_index1\\config.js");
-        FileUtil.writeBytes(bytes, file);
-        System.out.println("+++++++++++++  write file end  +++++++++++++");
-
         OSSProperty ossProperty = new OSSProperty();
         ossProperty.setEndpoint("https://oss-cn-beijing.aliyuncs.com");
         ossProperty.setAccessKeyId(System.getProperty("accessKeyID"));
@@ -53,27 +48,23 @@ public class MainTest200 {
         System.out.println("+++++++++++++  上传oss成功  +++++++++++++");
     }
 
-    private static void copyIndexName(File dir, File targetDir, JSONObject current){
-        FileUtil.deleteRecursively(targetDir);
+    private static void copyIndexName(File dir, JSONObject current){
         FileUtil.listFilesV2(dir, f ->{
-            File newPathFile = new File(f.getAbsolutePath().replace(dir.getAbsolutePath(), targetDir.getAbsolutePath()));
             if (!f.isHidden()) {
                 if (f.isDirectory() && !f.getName().contains("ignore")) {
-                    FileUtil.mkdir(newPathFile);
                     JSONObject jsonObject = new JSONObject();
                     jsonObject.put("id", idGenerator.getAndAdd(1));
                     jsonObject.put("parentId", current.getInteger("id"));
-                    jsonObject.put("name", newPathFile.getAbsolutePath());
+                    jsonObject.put("name", f.getAbsolutePath());
                     jsonObject.put("isFolder", true);
                     jsonArray.add(jsonObject);
-                    copyIndexName(f, newPathFile, jsonObject);
+                    copyIndexName(f, jsonObject);
                 }
                 if (f.isFile()) {
-                    FileUtil.touch(newPathFile + ".txt");
                     JSONObject jsonObject = new JSONObject();
                     jsonObject.put("id", idGenerator.getAndAdd(1));
                     jsonObject.put("parentId", current.getInteger("id"));
-                    jsonObject.put("name", newPathFile.getAbsolutePath());
+                    jsonObject.put("name", f.getAbsolutePath());
                     jsonObject.put("isFolder", false);
                     jsonArray.add(jsonObject);
 
