3fc1eaf113b08c1fc93bf45f906d19e5ef348cd0
youfang <fangyou@guiji.ai>
2024-04-28 09:46:01 +0800

    优化

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/ExtendEnvironmentPostProcessor.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/ExtendEnvironmentPostProcessor.java
@@ -8,10 +8,13 @@ import org.springframework.boot.logging.DeferredLogFactory;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.FileUrlResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
+import java.io.File;
 import java.net.URL;
 import java.util.Properties;
 
@@ -30,6 +33,23 @@ public class ExtendEnvironmentPostProcessor implements EnvironmentPostProcessor 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         try {
+            MutablePropertySources propertySources = environment.getPropertySources();
+            //项目resources扩展
+            ClassPathResource extendProperties = new ClassPathResource("extendProperties");
+            if (extendProperties.exists()) {
+                File extendPropertiesDir = extendProperties.getFile();
+                if (extendPropertiesDir.exists()) {
+                    File[] files = extendPropertiesDir.listFiles();
+                    if (files != null && files.length > 0) {
+                        for (File listFile : files) {
+                            Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("extendProperties" + File.separator + listFile.getName()));
+                            propertySources.addFirst(new PropertiesPropertySource("extendConfig_" + listFile.getName(), properties));
+                            log.info("扩展配置 加载成功！ 路径：[" + listFile.getPath() + "]");
+                        }
+                    }
+                }
+            }
+            //外部文件扩展
             String propertyPath = environment.getProperty("leo.extend-properties-path");
             if (StringUtils.isNotBlank(propertyPath)) {
                 Properties properties = new Properties();
@@ -38,12 +58,11 @@ public class ExtendEnvironmentPostProcessor implements EnvironmentPostProcessor 
                 } else {
                     properties = PropertiesLoaderUtils.loadProperties(new FileSystemResource(propertyPath));
                 }
-                MutablePropertySources propertySources = environment.getPropertySources();
                 propertySources.addFirst(new PropertiesPropertySource("extendConfig", properties));
             }
-            log.info("扩展配置加载成功！ 路径：[" + propertyPath + "]");
+            log.info("扩展配置 加载成功！ 路径：[" + propertyPath + "]");
         } catch (Exception e) {
-            log.error("扩展配置加载失败！ 原因：" + e.getClass().getName() + "[" + e.getMessage() + "]");
+            log.error("扩展配置 加载失败！ 原因：" + e.getClass().getName() + "[" + e.getMessage() + "]");
         }
     }
 }
