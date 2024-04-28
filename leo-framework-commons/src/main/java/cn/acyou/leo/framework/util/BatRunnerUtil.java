e610f9ce532ba6970abbf83f16762aca57d0a375
youfang <fangyou@guiji.ai>
2024-04-28 15:48:35 +0800

    Auto-commit

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/BatRunnerUtil.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/BatRunnerUtil.java
@@ -1,5 +1,9 @@
 package cn.acyou.leo.framework.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+
 /**
  * @author youfang
  * @version [1.0.0, 2024/4/28 15:42]
@@ -12,6 +16,10 @@ public class BatRunnerUtil {
             ProcessBuilder pb = new ProcessBuilder(batFilePath);
             Process process = pb.start();
             process.waitFor(); // 等待批处理脚本执行完成
+            InputStream inputStream = process.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            System.out.println(bufferedReader.readLine());
         } catch (Exception e) {
             e.printStackTrace();
         }
