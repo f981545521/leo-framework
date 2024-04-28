24878ebd6221aa8131959eb88364c02b7fb70b45
youfang <fangyou@guiji.ai>
2024-04-28 15:54:04 +0800

    Auto-commit

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/BatRunnerUtil.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/BatRunnerUtil.java
@@ -1,27 +1,35 @@
 package cn.acyou.leo.framework.util;
 
-import org.apache.commons.lang3.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.function.Consumer;
 
 /**
  * @author youfang
  * @version [1.0.0, 2024/4/28 15:42]
  **/
+@Slf4j
 public class BatRunnerUtil {
     public static void main(String[] args) {
+        String batFilePath = "D:\\workspace\\work\\auto-commit.bat";
+        exec(batFilePath, s ->{
+            System.out.println(s);
+        });
+    }
+
+    public static void exec(String runFilePath, Consumer<String> produce) {
         try {
-            // 替换为你的批处理文件路径
-            String batFilePath = "D:\\workspace\\work\\auto-commit.bat";
-            ProcessBuilder pb = new ProcessBuilder(batFilePath);
+            ProcessBuilder pb = new ProcessBuilder(runFilePath);
             Process process = pb.start();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                produce.accept(line);
             }
             int exitCode = process.waitFor();// 等待批处理脚本执行完成
-            System.out.println("执行结束：" + exitCode);
+            log.info("脚本 [{}] 执行结束：{}", runFilePath, exitCode);
         } catch (Exception e) {
             e.printStackTrace();
         }
