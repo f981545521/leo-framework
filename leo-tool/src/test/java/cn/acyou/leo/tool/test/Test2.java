f7e1f344fc88d046bab91185b7f4b76b54e2925f
youfang <fangyou@guiji.ai>
2024-05-22 10:59:33 +0800

    优化

--- leo-tool/src/test/java/cn/acyou/leo/tool/test/Test2.java
+++ leo-tool/src/test/java/cn/acyou/leo/tool/test/Test2.java
@@ -2,10 +2,12 @@ package cn.acyou.leo.tool.test;
 
 import cn.acyou.leo.framework.util.DateUtil;
 import cn.acyou.leo.framework.util.RandomUtil;
+import cn.acyou.leo.framework.util.StringUtils;
 import cn.hutool.core.thread.ConcurrencyTester;
 import cn.hutool.http.HttpUtil;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -49,5 +51,13 @@ public class Test2 {
         System.out.println(RandomUtil.randomNumbersInRange(1, 10, 10));
         System.out.println(RandomUtil.randomNumbersInRange(1, 10, 5));
         System.out.println(RandomUtil.randomNumbersInRange(1, 10, 9));
+    }
+
+    @Test
+    public void test34535(){
+        System.out.println(StringUtils.toChineseMoney(new BigDecimal("2134.23")));
+        System.out.println(StringUtils.toChineseMoney(new BigDecimal("12134.23")));
+        System.out.println(StringUtils.toChineseMoney(new BigDecimal("122134.23")));
+        System.out.println(StringUtils.toChineseMoney(new BigDecimal("3122134.23")));
     }
 }
