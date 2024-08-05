39196c362ee2ecf904af48a723087bb38dba4603
youfang <fangyou@guiji.ai>
2024-08-05 17:22:42 +0800

    优化

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/ExcelUtil.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/util/ExcelUtil.java
@@ -136,26 +136,28 @@ public class ExcelUtil {
         dataMap.forEach((sheetName, dataList)->{
             HSSFSheet sheet = workbook.createSheet(sheetName);
             sheet.setDefaultRowHeightInPoints((short) 20);
-            Map<String, CellStyle> styles = createStyles(workbook);
-            HSSFRow row = sheet.createRow(0);
-            Map<String, Object> map = dataList.get(0);
-            String[] tableHeaders = map.keySet().toArray(new String[0]);
-            // 创建表头
-            for (int i = 0; i < tableHeaders.length; i++) {
-                sheet.setColumnWidth(i, 20 * 256);
-                HSSFCell cell = row.createCell(i);
-                cell.setCellValue(tableHeaders[i]);
-                cell.setCellStyle(styles.get("header"));
-            }
-            // 写入数据
-            for (int i = 0; i < dataList.size(); i++) {
-                Map<String, Object> rowMap = dataList.get(i);
-                row = sheet.createRow(i + 1);
-                for (int j = 0; j < tableHeaders.length; j++) {
-                    HSSFCell cell = row.createCell(j);
-                    final Object o = rowMap.get(tableHeaders[j]);
-                    writeCell(cell, o);
-                    cell.setCellStyle(styles.get("data2"));
+            if (dataList != null && dataList.size() > 0) {
+                Map<String, CellStyle> styles = createStyles(workbook);
+                HSSFRow row = sheet.createRow(0);
+                Map<String, Object> map = dataList.get(0);
+                String[] tableHeaders = map.keySet().toArray(new String[0]);
+                // 创建表头
+                for (int i = 0; i < tableHeaders.length; i++) {
+                    sheet.setColumnWidth(i, 20 * 256);
+                    HSSFCell cell = row.createCell(i);
+                    cell.setCellValue(tableHeaders[i]);
+                    cell.setCellStyle(styles.get("header"));
+                }
+                // 写入数据
+                for (int i = 0; i < dataList.size(); i++) {
+                    Map<String, Object> rowMap = dataList.get(i);
+                    row = sheet.createRow(i + 1);
+                    for (int j = 0; j < tableHeaders.length; j++) {
+                        HSSFCell cell = row.createCell(j);
+                        final Object o = rowMap.get(tableHeaders[j]);
+                        writeCell(cell, o);
+                        cell.setCellStyle(styles.get("data2"));
+                    }
                 }
             }
         });
