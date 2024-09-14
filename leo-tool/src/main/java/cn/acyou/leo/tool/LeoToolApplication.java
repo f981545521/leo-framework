d431c941e421b0437744fe3664a9775ca63606c9
youfang <fangyou@guiji.ai>
2024-09-14 09:08:49 +0800

    优化

--- leo-tool/src/main/java/cn/acyou/leo/tool/LeoToolApplication.java
+++ leo-tool/src/main/java/cn/acyou/leo/tool/LeoToolApplication.java
@@ -1,16 +1,13 @@
 package cn.acyou.leo.tool;
 
 import cn.acyou.leo.framework.auto.EnableLeoFramework;
-import org.apache.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableLeoFramework
-@SpringBootApplication(scanBasePackages = "cn.acyou.leo", exclude = {
-        SpringBootConfiguration.class /*不使用shardingsphere的时候排除*/
-})
+@SpringBootApplication(scanBasePackages = "cn.acyou.leo")
 @EnableFeignClients
 @MapperScan({"cn.acyou.leo.tool.mapper"})
 public class LeoToolApplication {
