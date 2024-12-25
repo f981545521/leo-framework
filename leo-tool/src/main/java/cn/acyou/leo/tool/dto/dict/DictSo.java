8a5d121329d82ff768e0109a01b64338a2a665e2
youfang <981545521@qq.com>
2024-12-25 21:09:35 +0800

    add example

--- leo-tool/src/main/java/cn/acyou/leo/tool/dto/dict/DictSo.java
+++ leo-tool/src/main/java/cn/acyou/leo/tool/dto/dict/DictSo.java
@@ -1,7 +1,12 @@
 package cn.acyou.leo.tool.dto.dict;
 
 import cn.acyou.leo.framework.model.PageSo;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author youfang
@@ -25,4 +30,29 @@ public class DictSo extends PageSo {
      * 状态:  1-正常
      */
     private Integer status;
+
+    /**
+     *   "createDates":["2024-12-01","2024-12-25"]
+     */
+    @ApiModelProperty("创建时间查询")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private List<Date> createDates;
+
+    /**
+     * Mybatis参数错误
+     *
+     * It was either not specified and/or could not be found for the javaType ([Ljava.util.Date;) : jdbcType (null) combination.
+     *
+     * 需要特殊处理转换类型
+     * <if test="createDateArray!=null and createDateArray.length== 2">and create_time between #{createDateArray[0],javaType=java.util.Date,jdbcType=TIMESTAMP} and #{createDateArray[1],javaType=java.util.Date,jdbcType=TIMESTAMP}</if>
+     *
+     *
+     */
+    @ApiModelProperty("创建时间查询")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date[] createDateArray;
+
+    @ApiModelProperty("创建时间查询")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date[] createDateArray2;
 }
