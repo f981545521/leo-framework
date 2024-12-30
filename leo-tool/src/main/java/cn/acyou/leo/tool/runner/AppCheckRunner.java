a900bc010c1c1a868d913486d33f1b3f6dde734e
youfang <youfang@acyou.cn>
2024-12-30 13:36:52 +0800

    add

---
+++ leo-tool/src/main/java/cn/acyou/leo/tool/runner/AppCheckRunner.java
@@ -0,0 +1,18 @@
+package cn.acyou.leo.tool.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author youfang
+ * @version [1.0.0, 2020/7/1]
+ **/
+@Component
+public class AppCheckRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) {
+
+    }
+}
