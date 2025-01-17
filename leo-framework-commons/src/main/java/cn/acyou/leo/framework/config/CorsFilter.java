d0536c62390b9d2467d00954716e20cbde416b62
youfang <youfang@acyou.cn>
2025-01-17 10:20:51 +0800

    Cors配置优化

--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/CorsFilter.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/CorsFilter.java
@@ -1,7 +1,8 @@
 package cn.acyou.leo.framework.config;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 import org.springframework.web.cors.*;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,58 +11,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
- * 单独配置：Cors Filter
+ * 单独配置：CorsFilter 使用CorsFilter解决解决跨域
  *
- * 使用CorsFilter解决解决跨域
- * 参考：{@link org.springframework.web.filter.CorsFilter}
  * @author youfang
- * @version [1.0.0, 2020/7/28]
+ * @version [1.0.0, 2025/1/17]
  **/
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter extends OncePerRequestFilter {
 
     private final CorsConfigurationSource configSource;
 
-    private CorsProcessor processor = new DefaultCorsProcessor();
+    private final CorsProcessor processor = new DefaultCorsProcessor();
 
     public CorsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        List<String> list = new ArrayList<>();
-        list.add("*");
-        corsConfiguration.setAllowedOrigins(list);
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        //不接收Cookie
-        //        xhrFields: {
-        //            withCredentials: false
-        //        },
-        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.setExposedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", corsConfiguration);
         this.configSource = source;
     }
 
-
-    /**
-     * Configure a custom {@link CorsProcessor} to use to apply the matched
-     * {@link CorsConfiguration} for a request.
-     * <p>By default {@link DefaultCorsProcessor} is used.
-     */
-    public void setCorsProcessor(CorsProcessor processor) {
-        Assert.notNull(processor, "CorsProcessor must not be null");
-        this.processor = processor;
-    }
-
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-
         CorsConfiguration corsConfiguration = this.configSource.getCorsConfiguration(request);
         boolean isValid = this.processor.processRequest(corsConfiguration, request, response);
         if (!isValid || CorsUtils.isPreFlightRequest(request)) {
--- leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/LeoWebMvcConfig.java
+++ leo-framework-commons/src/main/java/cn/acyou/leo/framework/config/LeoWebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +19,17 @@ import java.util.List;
 @Configuration
 @Slf4j
 public class LeoWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //这里是Web容器层次的跨域
+        //registry.addMapping("/**")
+        //        .allowCredentials(false)
+        //        .allowedOriginPatterns("*")
+        //        .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+        //        .allowedHeaders("*")
+        //        .exposedHeaders("*");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
