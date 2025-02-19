eea12a093121544302ce7a51f52aef78d106185f
youfang <youfang@acyou.cn>
2025-02-19 10:57:08 +0800

    结构调整

--- leo-order/leo-order-mapper/src/main/java/cn/acyou/leo/order/mapper/StudentMapper.java
+++ leo-order/leo-order-mapper/src/main/java/cn/acyou/leo/order/mapper/StudentMapper.java
@@ -2,11 +2,13 @@ package cn.acyou.leo.order.mapper;
 
 import cn.acyou.leo.framework.mapper.Mapper;
 import cn.acyou.leo.order.entity.Student;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author youfang
  * @date 2018-04-15 下午 07:37
  **/
+@Repository
 public interface StudentMapper extends Mapper<Student> {
 
 
--- leo-product/leo-product-mapper/src/main/java/cn/acyou/leo/product/mapper/StudentMapper.java
+++ leo-product/leo-product-mapper/src/main/java/cn/acyou/leo/product/mapper/StudentMapper.java
@@ -2,11 +2,13 @@ package cn.acyou.leo.product.mapper;
 
 import cn.acyou.leo.framework.mapper.Mapper;
 import cn.acyou.leo.product.entity.Student;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author youfang
  * @date 2018-04-15 下午 07:37
  **/
+@Repository
 public interface StudentMapper extends Mapper<Student> {
 
 
