e0562741a3d2bfd73df2b1d55a5fe01bb039f7f2
youfang <youfang@acyou.cn>
2025-03-05 10:21:29 +0800

    update

--- leo-tool/src/main/java/cn/acyou/leo/tool/runner/AppCheckRunner.java
+++ leo-tool/src/main/java/cn/acyou/leo/tool/runner/AppCheckRunner.java
@@ -23,26 +23,33 @@ public class AppCheckRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         //测试返回多个值
         //test1();
+        //test2();
+    }
 
+    private void test2(){
+        //直接执行脚本返回
+        redisUtils.getRedisTemplate().execute(new DefaultRedisScript<>("" +
+                "local v = tonumber(ARGV[1])\r\n" +
+                "return v", String.class), new ArrayList<>(), "202503");
     }
 
     private void test1() {
         DefaultRedisScript<List> script = new DefaultRedisScript<>();
         script.setLocation(new ClassPathResource("lua/member_growth.lua"));
         script.setResultType(List.class);
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202401", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202402", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202403", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202404", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202405", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202406", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202407", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202408", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202409", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202410", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202411", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202412", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202501", "10", "MEMBER_GROWTH_KEY:1000");
-        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202502", "10", "MEMBER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202401", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202402", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202403", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202404", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202405", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202406", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202407", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202408", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202409", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202410", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202411", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202412", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202501", "10", "USER_GROWTH_KEY:1000");
+        redisUtils.getRedisTemplate().execute(script, new ArrayList<>(), "202502", "10", "USER_GROWTH_KEY:1000");
     }
 }
