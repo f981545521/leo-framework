5b88a494b76fb6d6a025f5799a7b49637052eb7c
fangyou <fangyou@xlbsoft.com>
2026-01-13 16:37:36 +0800

    test

--- leo-tool/src/test/java/cn/acyou/leo/tool/test/MainTest94.java
+++ leo-tool/src/test/java/cn/acyou/leo/tool/test/MainTest94.java
@@ -3,6 +3,9 @@ package cn.acyou.leo.tool.test;
 import cn.acyou.leo.framework.util.JSONUtil;
 import com.alibaba.fastjson.JSONObject;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * @author youfang
  * @version [1.0.0, 2024/5/16 10:18]
@@ -17,5 +20,15 @@ public class MainTest94 {
         jsonObject1.put("name", "小飞");
 
         System.out.println(JSONUtil.merge(jsonObject, jsonObject1, false));
+
+        String l = null;
+
+        System.out.println(l + "|" + "ok");
+
+        BigDecimal refundMoney = new BigDecimal("8.89");
+        BigDecimal handlingFeeMoney = refundMoney.multiply(new BigDecimal("33").multiply(new BigDecimal("0.01"))).setScale(2, RoundingMode.FLOOR);
+        System.out.println(refundMoney + "|" + handlingFeeMoney);
+
+
     }
 }
